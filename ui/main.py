d374aeb82145cfe3fa3584258c6895c4d2b568b5
czzzw彡 <9261137+cheng_zhiwei@user.noreply.gitee.com>
2024-07-22 18:01:31 +0800

    解决报错

--- ui/main.py
+++ ui/main.py
@@ -1,10 +1,18 @@
 import sys
 
 from PySide6.QtWidgets import QMainWindow, QPushButton, QWidget, QApplication
-class File_Manager(QMainWindow):
+from 计算器 import Ui_Form
+class File_Manager(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("File Manager")
+        self.setupUi(self)
+        self.setWindowTitle("计算器")
+        self.bind()
+
+    def bind(self):
+
+        pass
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
